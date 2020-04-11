9db1b7e5ef29b35eac2242942e4bbe6eba2852d9
RazzSunuwar <razzkoinch5@gmail.com>
2020-04-11 22:33:40 +0545

    Reducers

--- index.js
+++ index.js
@@ -7,3 +7,21 @@ function BUY_CAKE(){
     };
 };
 
+// (previousState, action) => newState
+
+const initialState = {
+    numOfCakes: 10
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type){
+        case BUY_CAKE: return{
+            ...state,
+            numOfCakes: state.numOfCakes - 1
+        };
+
+        default: return state;
+    };
+};
+
+
