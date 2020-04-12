336bd66396b29ca83088a10fe3e8fc7677372e59
RazzSunuwar <razzkoinch5@gmail.com>
2020-04-12 20:20:32 +0545

    Store

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/Store';
 import './App.css';
 import CakeContainer from './components/CakeContainer';
 
 function App() {
   return (
-    <div className="App">
-      <CakeContainer />
-    </div>
+    <Provider store = { store }>
+      <div className="App">
+        <CakeContainer />
+      </div>
+    </Provider>
   );
 }
 
